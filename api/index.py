c9bca4d30a95b49ae5a3b3eb3c9588f5bf22a5e9
Denistan <denistanb05@gmail.com>
2025-04-06 09:16:36 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -269,6 +269,6 @@ def upload_files():
     return jsonify(response)
 
 # Local testing only (ignored when running with Gunicorn)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
